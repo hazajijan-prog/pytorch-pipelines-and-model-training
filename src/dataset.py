505aaa7432b3160b3949d20c3a2ac877e5ca98bc
Haza Jijan <hazajijan@gmail.com>
2026-02-17 19:55:11 +0100

    Use global configured data target folder

--- src/dataset.py
+++ src/dataset.py
@@ -1,18 +1,18 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import torchvision
+from src.settings import DATA_DIR
 
 def get_dataloaders(batch_size=64):
-
     train_dataset = torchvision.datasets.CIFAR10(
-        root="../data",
+        root=DATA_DIR,
         train=True,
         transform=transforms.ToTensor(),
         download=True
     )
 
     test_dataset = torchvision.datasets.CIFAR10(
-        root="data",
+        root=DATA_DIR,
         train=False,
         transform=transforms.ToTensor(),
         download=True
